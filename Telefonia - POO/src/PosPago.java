bcac27fc07ca2aefb6d4456562933057721cd3d9
Felipe Nascimento <93695463+Felipee-Nascimento@users.noreply.github.com>
2023-05-12 18:32:01 -0300

    Update PosPago.java

--- Telefonia - POO/src/PosPago.java
+++ Telefonia - POO/src/PosPago.java
@@ -1,11 +1,35 @@
+import java.util.GregorianCalendar;
 
 public class PosPago extends Assinante{
 	
 	private float assinatura;
 
-	public PosPago(long cpf, String nome, int numero) {
+	public PosPago(long cpf, String nome, int numero, float assinatura) {
 		super(cpf, nome, numero);
-		// TODO Auto-generated constructor stub
+		this.assinatura = assinatura;
+	}
+	
+	public void fazerChamada(GregorianCalendar data, int duracao) {
+		
+		float valorMinuto = 1.04f; 
+		float valorChamada = valorMinuto*duracao; 
+		
+		if (chamadas.length>numChamadas) {
+			
+			Chamada chamada = new Chamada(data, duracao);
+			chamadas[numChamadas] = chamada;
+			numChamadas++;
+		}
+		else {
+			
+			System.out.println("Não foi possível realizar a chamada!");
+			
+		}
+	}
+	
+	public void imprimirFatura(int mes) {
+		
+		
 	}
 
 }
