61cfca41fd168533cfdb0f941c52cdcf185bfaef
Felipe Nascimento <93695463+Felipee-Nascimento@users.noreply.github.com>
2023-05-12 18:30:55 -0300

    Update Telefonia.java

--- Telefonia - POO/src/Telefonia.java
+++ Telefonia - POO/src/Telefonia.java
@@ -3,9 +3,52 @@ public class Telefonia {
 
 	private int numPrePagos;
 	private int numPosPagos;
+	private PrePago[] prePagos;
+	private PosPago[] posPagos;
+	
+	
+	public void cadastrarAssinante() {
+		
+		
+		
+	}
+	
+	public void listarAssinantes() {
+		
+		
+	}
+	
+	public void fazerChamada() {
+		
+		
+	}
+	
+	public void fazerRecarga() {
+		
+		
+	}
+	
+	public PrePago localizarPrePago(long cpf) {
+		
+		
+	}
+	
+	public PosPago localizarPosPago(long cpf) {
+		
+		
+	}
+	
+	
+	public void imprimirFaturas() {
+		
+		
+		
+		
+	}
+	
 	
 	public static void main(String [] args) {
 		
-		
+		Telefonia telefonia = new Telefonia(); 
 	}
 }
