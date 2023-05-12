a00988eb84656ce73a092ee57cc1c25c491f4d21
Felipe Nascimento <93695463+Felipee-Nascimento@users.noreply.github.com>
2023-05-12 18:32:50 -0300

    Update PrePago.java

--- Telefonia - POO/src/PrePago.java
+++ Telefonia - POO/src/PrePago.java
@@ -1,14 +1,54 @@
+import java.util.GregorianCalendar;
 
 public class PrePago extends Assinante{
 
-	private float creditos;
-	private int numRecargas;
-	
-	public PrePago(long cpf, String nome, int numero) {
+	 private Recarga[] recargas;
+	 private int numRecargas;
+	 private float creditos;
+
+	public PrePago(long cpf, String nome, int numero, Recarga[] recargas, int numRecargas, float creditos) {
 		super(cpf, nome, numero);
-		// TODO Auto-generated constructor stub
+		this.recargas = new Recarga[100];
+		this.numRecargas = numRecargas;
+		this.creditos = creditos;
 	}
 	
+	public void fazerChamada(GregorianCalendar data, int duracao){
+		
+		float valorMinuto = 1.45f; 
+		float valorChamada = valorMinuto*duracao; 
+		
+		if (chamadas.length>numChamadas && creditos>valorChamada) {
+			
+			Chamada chamada = new Chamada(data, duracao);
+			chamadas[numChamadas] = chamada;
+			numChamadas++;
+			creditos = creditos-valorChamada;
+		}
+		else {
+			
+			System.out.println("Não foi possível realizar a chamada!");
+			
+		}
+	}
 	
+	public void recarregar(GregorianCalendar data, float valor) {
+		
+		if(recargas.length<=100) {
+			
+			Recarga recarga = new Recarga(data, valor);
+					recargas[numRecargas] = recarga;
+					numRecargas++;
+					creditos=creditos+valor;
+		}
+		else {
+			System.out.println("Não foi possível realizar a recarga!");
+		}
+		
+	}
 	
+	public void imprimirFatura(int mes) {
+		
+		
+	}
 }
