fc928a9bb6fb21e96ce8eda68f0265f8bf6afa9a
Felipe Nascimento <93695463+Felipee-Nascimento@users.noreply.github.com>
2023-05-20 17:07:41 -0300

    Update Assinante.java

--- Telefonia - POO/src/Assinante.java
+++ Telefonia - POO/src/Assinante.java
@@ -1,26 +1,36 @@
-
 public class Assinante {
 	
-	 private long cpf;
-	    private String nome;
-	    private int numero;
-	    protected Chamada[] chamadas;
-	    protected int numChamadas;
+    private long cpf;
+    private String nome;
+    private int numero;
+    protected Chamada[] chamadas;
+    protected int numChamadas;
 
 
-	    public Assinante(long cpf, String nome, int numero) {
-	        this.cpf = cpf;
-	        this.nome = nome;
-	        this.numero = numero;
-	        this.chamadas = new Chamada[10];
-	    }
+    public Assinante(long cpf, String nome, int numero) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.numero = numero;
+        this.chamadas = new Chamada[10];
+    }
 
-	    public long getCpf() {
-	        return cpf;
-	    }
-	    
-	    @Override
-	    public String toString() {
-	        return "CPF: " + this.cpf + ", Nome: " + this.nome + ", Numero: " + this.numero;
-	    }
+    public long getCpf() {
+        return cpf;
+    }
+    
+    public String getNome() {
+		return nome;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+
+	@Override
+    public String toString() {
+        return "CPF: " + this.cpf + ", Nome: " + this.nome + ", Numero: " + this.numero;
+    }
+	
+ 
 }
