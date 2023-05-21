477b254cce0d13a5f035e7f1402eaabbd9c54245
Felipe Nascimento <93695463+Felipee-Nascimento@users.noreply.github.com>
2023-05-21 15:27:52 -0300

    Update PosPago.java

--- Telefonia - POO/src/PosPago.java
+++ Telefonia - POO/src/PosPago.java
@@ -29,9 +29,23 @@ public class PosPago extends Assinante{
 	
 	public void imprimirFatura(int mes) {
 		
+		float totalFatura=0;
+		
 		System.out.println("CPF: " + getCpf());
 		System.out.println("Nome: " + getNome());
 		System.out.println("Numero: " + getNumero());
+		
+		Chamada[] c = chamadas;
+		for(int i=0;i<chamadas.length;i++) {
+			System.out.println("Data: "+c[i].getData().getTime());
+			System.out.println("Duracao: "+c[i].getDuracao());
+			float custo_ligacao = c[i].getDuracao() * 1.04f; 
+			System.out.println("Custo: R$ "+custo_ligacao);
+			totalFatura = totalFatura + custo_ligacao;
+		}
+
+		System.out.println("Valor da Assinatura: R$ "+this.assinatura);
+		System.out.println("Total da Fatura: R$"+(totalFatura+this.assinatura));
 		
 	}
 
